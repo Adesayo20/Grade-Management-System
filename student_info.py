1024c38a82ef7f037e85f3dc558db896cea80512
Adesayo M <105006406+Adesayo20@users.noreply.github.com>
2024-10-23 18:53:31 +0100

    Update student_info.py

--- student_info.py
+++ student_info.py
@@ -1,22 +1,22 @@
 students = {
     'John Doe': [
-        ('Math', 85),
-        ('Science', 90),
-        ('History', 78)
+        ['Math', 85],
+        ['Science', 90],
+        ['History', 78]
     ],
     'jane smith': [
-        ('Math', 35),
-        ('Science', 45),
-        ('History', 60)
+        ['Math', 35],
+        ['Science', 45],
+        ['History', 60]
     ],
     'Emily Davis': [
-        ('Math', 95),
-        ('Science', 92),
-        ('History', 88)
+        ['Math', 85],
+        ['Science', 90],
+        ['History', 28]
     ],
     'Michael Brown': [
-        ('Math', 55),
-        ('Science', 60),
-        ('History', 58)
+        ['Math', 85],
+        ['Science', 90],
+        ['History', 79]
     ]
 }
